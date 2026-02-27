bc5a5266ebdaf310c878ccc055b9b2de94e89a11
Mbas <mbas750@gmail.com>
2026-02-27 05:20:32 +0100

    Update routes for confectionery and electronics to use root path

--- mock-srv/routes/confectionery/index.js
+++ mock-srv/routes/confectionery/index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 module.exports = async function (fastify, opts) {
-  fastify.get('/confectionery', async function (request, reply) {
+  fastify.get('/', async function (request, reply) {
     return [
       {id: 'B1', name: 'Chocolate Bar', rrp: '22.40', info: 'Delicious overpriced chocolate.'}
     ]
